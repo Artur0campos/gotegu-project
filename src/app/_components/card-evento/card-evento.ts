f22aad2c911cbb71ac15b3cad83d8f49ca165c7b
Artur0campos <arturcamposcamara@gmail.com>
2026-03-05 16:36:24 -0300

    card-evento component done

---
+++ src/app/_components/card-evento/card-evento.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-evento',
+  imports: [],
+  templateUrl: './card-evento.html',
+  styleUrl: './card-evento.css',
+})
+export class CardEvento {
+
+}
