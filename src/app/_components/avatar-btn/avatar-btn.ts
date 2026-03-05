7577d8d9ed9fed75b0ec1407e67c99cc80c05c72
Artur0campos <arturcamposcamara@gmail.com>
2026-03-05 17:00:00 -0300

    container and avatar component done

---
+++ src/app/_components/avatar-btn/avatar-btn.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar-btn',
+  imports: [],
+  templateUrl: './avatar-btn.html',
+  styleUrl: './avatar-btn.css',
+})
+export class AvatarBtn {
+
+}
---
+++ src/app/_components/container-main/container-main.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ɵEmptyOutletComponent } from "@angular/router";
+
+@Component({
+  selector: 'app-container-main',
+  imports: [ɵEmptyOutletComponent],
+  templateUrl: './container-main.html',
+  styleUrl: './container-main.css',
+})
+export class ContainerMain {
+
+}
