150c7d5a7ebd1298215586bf3d91b76dc1be0476
Artur0campos <arturcamposcamara@gmail.com>
2026-03-05 17:01:16 -0300

    .

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./_components/header/header";
+import { CardEvento } from "./_components/card-evento/card-evento";
+import { ContainerMain } from "./_components/container-main/container-main";
+import { AvatarBtn } from "./_components/avatar-btn/avatar-btn";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, CardEvento, ContainerMain, AvatarBtn],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
