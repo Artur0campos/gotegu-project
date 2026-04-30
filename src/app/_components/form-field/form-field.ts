783a21fc5699b7769be75e30a9f6bb43011fb0c8
Artur Campos Câmara <117042395+Artur0campos@users.noreply.github.com>
2026-04-30 15:48:05 -0300

    feat: new component named: input-component, and fixing some stuffs

--- src/app/_components/form-field/form-field.ts
+++ src/app/_components/form-field/form-field.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { InputComponent } from '../input-component/input-component';
+import { PrimaryBtn } from '../primary-btn/primary-btn';
+
 
 @Component({
   selector: 'app-form-field',
-  imports: [],
+  imports: [InputComponent, PrimaryBtn],
   templateUrl: './form-field.html',
   styleUrl: './form-field.css',
 })
